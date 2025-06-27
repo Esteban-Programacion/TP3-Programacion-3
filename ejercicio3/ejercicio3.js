0f2b4ee1bf28677a89320647b205dec94e3d9692
Esteban Ricardo Ormeño <estebanormeno116@gmail.com>
2025-06-27 03:35:12 -0300

    Commit del ejercicio 3

---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,23 @@
+const btnCargar = document.getElementById("btncargar");
+const listaTareas = document.getElementById("listadetareas");
+
+btnCargar.addEventListener("click", async () => {
+  listaTareas.innerHTML = ""; 
+
+  const respuesta = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+  if (!respuesta.ok) {
+    listaTareas.innerHTML = `<li style="color:red;">Error: ${respuesta.status}</li>`;
+    return;
+  }
+
+  const tareas = await respuesta.json();
+
+  const tareasCompletadas = tareas.filter(t => t.completed);
+
+  tareasCompletadas.forEach(tarea => {
+    const li = document.createElement("li");
+    li.textContent = tarea.title;
+    listaTareas.appendChild(li);
+  });
+});
